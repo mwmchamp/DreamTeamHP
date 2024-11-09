94d21ab02350d805b57f55c243aca97e36e98dab
neilmakur <neilmakur@gmail.com>
2024-11-09 17:08:06 -0500

    Arduino Code updated to flash etc

--- hardware/Arduino_Code/Arduino_Code.ino
+++ hardware/Arduino_Code/Arduino_Code.ino
@@ -8,6 +8,8 @@ const long interval = 2000; // 2 seconds
 int currentR = 0, currentG = 255, currentB = 0;
 int targetR, targetG, targetB;
 
+bool taken;
+
 void setup() {
   // Initialize the LCD
   lcd.begin(16, 2); // Set up the LCD's number of columns and rows
@@ -17,36 +19,53 @@ void setup() {
   pinMode(4, INPUT);
 }
 
-void loop() {
-  unsigned long currentMillis = millis();
-
-  if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;
-
-    // Set new random target color every 2 seconds
-    targetR = random(256);
-    targetG = random(256);
-    targetB = random(256);
+int mins_till(next_hrs, next_mins, cur_hrs, cur_mins) {
+  int hrs_diff = (next_hrs - cur_hrs + 24) % 24;
+  int mins_diff = next_mins - cur_mins;
+  if(hrs_diff == 0 && mins_dif < 0){
+    hrs_diff = 24;
   }
+  return hrs_diff * 60 + mins_diff;
+}
 
-  // Gradually fade to the target color
-  if (currentR < targetR) currentR++;
-  else if (currentR > targetR) currentR--;
+void set_color(int time_left, int cur_secs){
+  int R = 256 * (600 - 60 * time_left - cur_secs) / 600;
+  int G = 256 * (60 * time_left + cur_secs) / 60;
+  lcd.setRGB(R, G, 0);
+}
 
-  if (currentG < targetG) currentG++;
-  else if (currentG > targetG) currentG--;
+void flash() {
 
-  if (currentB < targetB) currentB++;
-  else if (currentB > targetB) currentB--;
+}
 
-  lcd.setRGB(currentR, currentG, currentB);
-
-  // Read the status of pin D4 and print it to the LCD
+void loop() {
+  
+  string next_pill;
+  int next_hrs;
+  int next_mins;
+  int cur_hrs;
+  int cur_mins;
+  int cur_secs;
+  // get these values + taken from bluetooth
+  
   lcd.clear();
-  if (digitalRead(4) == HIGH) {
-    lcd.print("D4 is HIGH");
+  lcd.setCursor(0, 0);
+  lcd.print(next_pill);
+  lcd.setCursor(0, 1);
+  lcd.print(next_hrs + ": " + next_mins);
+
+  int time_left = mins_till(next_hrs, next_mins, cur_hrs, cur_mins);
+
+  if(time_left <= 10 && !taken) {
+    set_color(time_left, cur_secs);
+    flash();
   } else {
-    lcd.print("D4 is LOW");
+    lcd.setRGB(0, 255, 0);
+  }
+  
+  if (digitalRead(4) == LOW) {
+    // Send that the pill has been taken
+    flash();
   }
 
   delay(25); // Small delay to control the fade speed
